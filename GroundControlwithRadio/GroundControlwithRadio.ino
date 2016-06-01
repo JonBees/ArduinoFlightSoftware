51ef5813a09df29f552f833de34b5e9628daeb5e
LunarLionGNC <lunarliongnc@gmail.com>
2016-06-01 09:50:40 -0400

    Removed "clearing"

--- GroundControlwithRadio/GroundControlwithRadio.ino
+++ GroundControlwithRadio/GroundControlwithRadio.ino
@@ -1,40 +1,32 @@
-long time = 0;
+int count = 0;
 void setup()  
 {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);//USB connection to PC with LabView
   Serial1.begin(9600);//Connection to XBee
   Serial2.begin(9600);//Connection to GroundControlWithoutRadio
-  //Serial.flush();
-  //Serial2.flush();
 }
 
 void loop() // run over and over
 {
-  if (Serial2.available()){
+  if(Serial2.available()){
     char c = Serial2.read();
-
-    if (c == '~'){
-      Serial1.clearing();
-     
-      Serial2.clearing();
+    if (c == 'h'){
+      count++;
+      Serial1.print(count);
     }
-    else{
       Serial1.write(c);
       Serial.write(c);
-    }
   }
-  else
-  {
-      Serial2.begin(9600);//Connection to GroundControlWithoutRadio
+  else{
+      Serial2.begin(9600);
   }
-  if (Serial1.available()){
+  
+  if(Serial1.available()){
     char d = Serial1.read();
-
     Serial2.write(d);
   }
-  else
-  {
-    Serial1.begin(9600);//Connection to XBee
+  else{
+    Serial1.begin(9600);
   }
 }
